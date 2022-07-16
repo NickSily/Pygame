03652698203b06d95979d548d2d85181585e84bf
Nicholas <nicholas.svasc@gmail.com>
2022-07-16 16:35:07 -0400

    First Commit

---
+++ 2dRPG.py
@@ -0,0 +1,26 @@
+import pygame
+import sys
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((1280, 720))
+        pygame.display.set_caption("2D RPG")
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            self.screen.fill('black')
+            pygame.display.update()
+            self.clock.tick(30)
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
